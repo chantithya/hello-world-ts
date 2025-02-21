f83ad3c63a31a252f4200bafca85a0aa6e3d08f0
Som Chantithya <somchantithya@Soms-MacBook-Pro.local>
2025-02-21 15:03:09 +0700

    Initial commit: TypeScript setup

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let user;
+user = ["Alice", 25, true];
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// let sales: number = 123456789;
+// let course: string = 'TypeScrip';
+// let is_published: boolean = true;
+// let level;
+
+
+// function render(document) {
+//     console.log(document);
+// }
+
+
+// Array
+
+
+// Tuple
+let user: [string, number, boolean];
+user = ["Alice", 25, true]; // ✅ Correct
+// user = [25, "Alice", true]; // ❌ Error: Order and types must match
